e77071193d45f7efe515fe5063a01ed151eb553c
fozziiee <kaydefoster@gmail.com>
2024-07-03 17:21:53 +1000

    feat: Add new components

---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Testimonials = () => {
+  return <div>Testismonials</div>;
+};
+
+export default Testimonials;
---
+++ src/components/index.js
@@ -0,0 +1,25 @@
+import Billing from "./Billing";
+import Business from "./Business";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Footer from "./Footer";
+import GetStarted from "./GetStarted";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+
+export {
+    Billing,
+    Business,
+    CardDeal,
+    Clients,
+    CTA,
+    Footer,
+    GetStarted,
+    Hero,
+    Navbar,
+    Stats,
+    Testimonials,
+};
